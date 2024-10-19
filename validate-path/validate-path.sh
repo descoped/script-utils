23fe7c2bc58dcd619d175dbf4b87ecbf7bce40ac
oranheim <oranheim@gmail.com>
2024-10-19 10:49:18 +0200

    Update Validate Path

--- validate-path/validate-path.sh
+++ validate-path/validate-path.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo $PATH | tr ':' '\n' | while read p; do if [ -z "$p" ]; then echo "Empty (::) path exists"; elif [ -e "$p" ]; then echo "$p exists"; else echo "$p does not exist"; fi; done
+echo $PATH | tr ':' '\n' | while read p
+do
+    if [ -z "$p" ]; then
+        echo "Empty (::) path exists"
+    elif [ -e "$p" ]; then
+        echo "$p exists"
+    else
+        echo "$p does not exist"
+    fi
+done
+
